9e0160249669d19653fb274fd4ed0f5acd1803b6
halilcanturan <c.turan0405@gmail.com>
2024-02-16 08:51:04 +0300

    Add project files.

---
+++ BlogApp/Data/Concrete/EfCore/SeedData.cs
@@ -0,0 +1,111 @@
+﻿using BlogApp.Entity;
+using Microsoft.EntityFrameworkCore;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace BlogApp.Data.Concrete.EfCore
+{
+    public static class SeedData
+    {
+        public static void TestVerileriniDoldur(IApplicationBuilder app)
+        {
+            var context = app.ApplicationServices.CreateScope().ServiceProvider.GetService<BlogContext>();
+
+            if (context != null)
+            {
+                if (context.Database.GetPendingMigrations().Any())
+                {
+                    context.Database.Migrate();
+                }
+
+                if (!context.Tags.Any())
+                {
+                    context.Tags.AddRange(
+                        new Tag { Text = "web programlama", Url = "web-programlama", Color = TagColors.warning },
+                        new Tag { Text = "backend", Url = "backend", Color = TagColors.info },
+                        new Tag { Text = "frontend", Url = "frontend", Color = TagColors.success },
+                        new Tag { Text = "fullstack", Url = "fullstack", Color = TagColors.secondary },
+                        new Tag { Text = "php", Url = "php", Color = TagColors.primary }
+                    );
+                    context.SaveChanges();
+                }
+
+                if (!context.Users.Any())
+                {
+                    context.Users.AddRange(
+                        new User { UserName = "canturan",Name="Can TURAN",Email="info@canturan.com",Password="123456", Image = "p1.jpg" },
+                        new User { UserName = "atlasturan",Name="Atlas TURAN",Email="info@atlasturan.com", Password = "123456", Image = "p2.jpg" }
+                    );
+                    context.SaveChanges();
+                }
+
+                if (!context.Posts.Any())
+                {
+                    context.Posts.AddRange(
+                        new Post
+                        {
+                            Title = "Asp.net core",
+                            Description = "Asp.Net Core dersleri",
+                            Content = "Asp.net core dersleri",
+                            Url = "aspnet-core",
+                            IsActive = true,
+                            PublishedOn = DateTime.Now.AddDays(-10),
+                            Tags = context.Tags.Take(3).ToList(),
+                            Image = "1.jpg",
+                            UserId = 1,
+                            Comments = new List<Comment> {
+                                new Comment {Text="iyi bir kurs", PublishedOn=new DateTime(),UserId=1 },
+                            new Comment {Text="çok faydalı bir kurs", PublishedOn=new DateTime(),UserId=2 }}
+                        },
+                        new Post
+                        {
+                            Title = "Php",
+                            Description= "Php core dersleri",
+                            Content = "Php core dersleri",
+                            Url = "php",
+                            IsActive = true,
+                            Image = "2.jpg",
+                            PublishedOn = DateTime.Now.AddDays(-20),
+                            Tags = context.Tags.Take(2).ToList(),
+                            UserId = 1
+                        },
+                        new Post
+                        {
+                            Title = "Django",
+                            Description = "Django dersleri",
+                            Content = "Django dersleri",
+                            Url = "djagno",
+                            IsActive = true,
+                            Image = "3.jpg",
+                            PublishedOn = DateTime.Now.AddDays(-15),
+                            Tags = context.Tags.Take(4).ToList(),
+                            UserId = 2
+                        },
+                         new Post
+                         {
+                             Title = "React Dersleri",
+                             Content = "React dersleri",
+                             Url = "react-dersleri",
+                             IsActive = true,
+                             Image = "3.jpg",
+                             PublishedOn = DateTime.Now.AddDays(-10),
+                             Tags = context.Tags.Take(4).ToList(),
+                             UserId = 2
+                         },
+                          new Post
+                          {
+                              Title = "Angular Dersleri",
+                              Content = "Angular dersleri",
+                              Url = "angular-dersleri",
+                              IsActive = true,
+                              Image = "3.jpg",
+                              PublishedOn = DateTime.Now.AddDays(-60),
+                              Tags = context.Tags.Take(4).ToList(),
+                              UserId = 2
+                          }
+                    );
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
